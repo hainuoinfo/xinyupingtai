434627dedf5ee4601426d01f325e7235cee65b8c
hainuo <admin@hainuo.info>
2014-07-01 12:23:23 +0800

    修正了mysql语法错误

--- fix_bf_task.sql
+++ fix_bf_task.sql
@@ -1,5 +1,5 @@
 ﻿ALTER TABLE bf_task ADD COLUMN shareimage  varchar(255) null  COMMENT '分享图' AFTER share;
 ALTER TABLE bf_task MODIFY pinimage varchar(255) NULL;
-ALTER TABLE  `bf_log` ADD  `fabudian` INT NULL DEFAULT NULL AFTER  `val` COMMENT  '麦点';
-ALTER TABLE  `bf_log` ADD  `tasktype` INT( 3 ) NOT NULL AFTER  `fabudian` COMMENT  '任务类型';
-ALTER TABLE  `bf_log` ADD  `totalmoney` INT NULL DEFAULT NULL AFTER  `fabudian` COMMENT  '存款';
+ALTER TABLE  `bf_log` ADD  `fabudian` INT NULL DEFAULT NULL   COMMENT  '麦点' AFTER  `val`;
+ALTER TABLE  `bf_log` ADD  `tasktype` INT( 3 ) NOT NULL  COMMENT  '任务类型' AFTER  `fabudian` ;
+ALTER TABLE  `bf_log` ADD  `totalmoney` INT NULL DEFAULT NULL   COMMENT  '存款' AFTER  `fabudian`;
