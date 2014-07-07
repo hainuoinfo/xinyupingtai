3511c83c9029f4632f8fe961c10ca9d595a18030
hainuo <admin@hainuo.info>
2014-07-07 20:40:25 +0800

    修正重要bug
    1、修正了不选择上传截图时，进度无法进入下一步的bug
    2、修正了积分记录显示页面显示bug 显示积分变动时的总积分
    3、修正了数据可增加字段是字段类型不正确的项目

--- fix_bf_task.sql
+++ fix_bf_task.sql
@@ -1,6 +1,6 @@
 ﻿ALTER TABLE bf_task ADD COLUMN shareimage  varchar(255) null  COMMENT '分享图' AFTER share;
 ALTER TABLE bf_task MODIFY pinimage varchar(255) NULL;
-ALTER TABLE  `bf_log` ADD  `fabudian` INT NULL DEFAULT NULL   COMMENT  '麦点' AFTER  `val`;
-ALTER TABLE  `bf_log` ADD  `tasktype` INT( 3 ) NOT NULL  COMMENT  '任务类型' AFTER  `fabudian` ;
-ALTER TABLE  `bf_log` ADD  `totalmoney` INT NULL DEFAULT NULL   COMMENT  '存款' AFTER  `fabudian`;
-ALTER TABLE `bf_log` ADD `totalcredits` INT NOT NULL DEFAULT '0' COMMENT '总积分数' AFTER `fabudian` 
+ALTER TABLE  `bf_log` ADD  `fabudian`  float(11,5) NOT NULL DEFAULT '0.00000'    COMMENT  '麦点' AFTER  `val`;
+ALTER TABLE  `bf_log` ADD  `tasktype` float(11,5) NOT NULL DEFAULT '0.00000'   COMMENT  '任务类型' AFTER  `fabudian` ;
+ALTER TABLE  `bf_log` ADD  `totalmoney` float(11,5) NOT NULL DEFAULT '0.00000'   COMMENT  '存款' AFTER  `fabudian`;
+ALTER TABLE `bf_log` ADD `totalcredits` float(11,5) NOT NULL DEFAULT '0.00000' COMMENT '总积分数' AFTER `fabudian` ;
