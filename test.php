5a4148a541017c39e642af348fdfbeb15e807d8b
hainuo <admin@hainuo.info>
2014-08-17 12:05:01 +0800

    首页增加问题答案登陆 Signed-off-by: hainuo <admin@hainuo.info>

--- test.php
+++ test.php
@@ -1,12 +1,1 @@
-	<?php //$uinfo[0]['uimg']
-		$type=getimagesize('http://static.blog.csdn.net/skin/ink/images/body_bg.jpg');//取得图片的大小，类型等
-		$content=file_get_contents('http://static.blog.csdn.net/skin/ink/images/body_bg.jpg');
-		$file_content=chunk_split(base64_encode($content));//base64编码
-		switch($type[2]){//判读图片类型
-			case 1:$img_type="gif";break;
-			case 2:$img_type="jpg";break;
-			case 3:$img_type="png";break;
-		}
-		$img='data:image/'.$img_type.';base64,'.$file_content;//合成图片的base64编码
-		echo "<img src='".$img."'>";
-	?>
+<div id="ajaxLoading" style="width: 100%;height: 100%;top: 0;left: 0;position: absolute;z-index: 9999;background:none repeat scroll 0 0 #CCCCCC;opacity: 0.4;filter:alpha(opacity=50)">Loading</div> 
